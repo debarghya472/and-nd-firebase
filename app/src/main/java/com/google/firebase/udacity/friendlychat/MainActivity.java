695c574f7b7caa430f30bfd0b4ebf77d56ed71f7
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-06-22 17:45:43 +0530

    test2

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -144,8 +144,6 @@ public class MainActivity extends AppCompatActivity {
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Send messages on click
-                Log.v("Bokk","bal");
                 FriendlyMessage friendlyMessage=new FriendlyMessage(mMessageEditText.getText().toString(),mUsername,null);
                 mMessagedatabaseReference.push().setValue(friendlyMessage);
                 // Clear input box
